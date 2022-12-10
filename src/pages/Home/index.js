244be912c01f97081543ea76736fc3e8b6f123e4
LeHa2505 <leha25052001@gmail.com>
2022-12-10 19:37:55 +0700

    homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,98 @@
+import style from './home.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(style);
+
 function Home() {
     return (
-        <h2>Home</h2>
-      );
+        <main className={cx('container')}>
+            <div className={cx('homepage-advertise', 'row')}>
+                <div className={cx('homepage-advertise-left', 'col-md-8')}>
+                    <div className={cx('homepage-carousel')}>
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                            <div class="carousel-indicators">
+                                <button
+                                    type="button"
+                                    data-bs-target="#carouselExampleIndicators"
+                                    data-bs-slide-to="0"
+                                    class="active"
+                                    aria-current="true"
+                                    aria-label="Slide 1"
+                                ></button>
+                                <button
+                                    type="button"
+                                    data-bs-target="#carouselExampleIndicators"
+                                    data-bs-slide-to="1"
+                                    aria-label="Slide 2"
+                                ></button>
+                                <button
+                                    type="button"
+                                    data-bs-target="#carouselExampleIndicators"
+                                    data-bs-slide-to="2"
+                                    aria-label="Slide 3"
+                                ></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img
+                                        src={require('../../assets/images/homepage_imgs/BitexT12_840x320.jpg')}
+                                        class="d-block w-100"
+                                        alt="..."
+                                    ></img>
+                                </div>
+                                <div class="carousel-item">
+                                    <img
+                                        src={require('../../assets/images/homepage_imgs/Megasale_Mainbanner_T12_Slide_840x320_Icanconnect.jpg')}
+                                        class="d-block w-100"
+                                        alt="..."
+                                    ></img>
+                                </div>
+                                <div class="carousel-item">
+                                    <img
+                                        src={require('../../assets/images/homepage_imgs/McBooksT12_840x320.jpg')}
+                                        class="d-block w-100"
+                                        alt="..."
+                                    ></img>
+                                </div>
+                            </div>
+                            <button
+                                class="carousel-control-prev"
+                                type="button"
+                                data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="prev"
+                            >
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button
+                                class="carousel-control-next"
+                                type="button"
+                                data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="next"
+                            >
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className={cx('homepage-advertise-right', 'col-md-4')}>
+                    <div className={cx('advertise-right-img')}>
+                        <img
+                            className={cx('right-img')}
+                            src={require('../../assets/images/homepage_imgs/up-right.jpg')}
+                        ></img>
+                    </div>
+                    <div className={cx('advertise-right-img')}>
+                        <img
+                            className={cx('right-img')}
+                            src={require('../../assets/images/homepage_imgs/bottom-right.png')}
+                        ></img>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default Home;
